45a8b522480ba97bad0098184d7022dbda45cedc
Andrew Ribeiro <andtankian@live.com>
2019-04-05 00:57:38 -0300

    Improved, implemented examples with theme changing.

--- src/index.js
+++ src/index.js
@@ -4,47 +4,98 @@ import { render } from "react-dom";
 import Button from './lib'
 
 import { FaAngleRight, FaAngleLeft } from 'react-icons/fa';
+import {ThemeProvider} from 'styled-components';
 
 const onClick = (e) => {
   console.log("clicked: ", e);
 }
 
-const App = () => (
-  <React.Fragment>
-    <Button onClick={onClick}>
-      Default
-    </Button>
-    <Button appearance='primary'>
-      Primary
-    </Button>
-    <Button size='small'>
-      Size Small
-    </Button>
-    <Button size='large'>
-      Size Large
-    </Button>
-    <Button disabled={true}>
-      Disabled
-    </Button>
-    <Button disabled={true} appearance='primary' onClick={onClick}>
-      Disabled Primary
-    </Button>
-    <Button loading={true}>
-      Loading
-    </Button>
-    <Button loading={true} appearance='primary' onClick={onClick}>
-      Loading Primary
-    </Button>
-    <Button afterIcon={<FaAngleRight/>}>Text Icon</Button>
-    <Button beforeIcon={<FaAngleLeft/>}>Text Icon Before</Button>
-    <Button afterIcon={<FaAngleRight/>} appearance='primary'/>
-    <Button afterIcon={<FaAngleRight/>} isCircle={true}/>
-    <Button afterIcon={<FaAngleRight/>} isCircle={true} appearance='primary'/>
-    <Button afterIcon={<FaAngleRight/>} isCircle={true} size='large'/>
-    <Button afterIcon={<FaAngleRight/>} isCircle={true} appearance='primary' size='small'/>
-    <Button afterIcon={<FaAngleRight/>} isCircle={true} size='large'>With Text </Button>
-    <Button afterIcon={<FaAngleRight/>} isCircle={true} appearance='primary' size='small'/>
-  </React.Fragment>
-);
+const buttonsArray = [{
+  text: 'Regular',
+  props: {}
+}, {
+  text: 'Small',
+  props: {}
+}, {
+  text: 'Large',
+  props: {}
+}, {
+  text: 'Primary',
+  props: {}
+}, {
+  text: 'Secondary',
+  props: {}
+}, {
+  text: 'OnClick',
+  props: {
+    onClick
+  }
+}];
+
+class App extends React.Component {
+
+  state = {
+    loading: false,
+    disabled: false,
+    theme: {
+      mode: 'light'
+    }
+  }
+
+  onChangeBasicProps = (input) => {
+    this.setState({
+      [input.target.id]: input.target.checked
+    });
+  }
+
+  onChangeTheme = (input) => {
+    this.setState({
+      theme:{
+        mode: input.target.id
+      }
+    });
+
+  }
+
+
+  render() {
+    return (
+      <ThemeProvider theme={this.state.theme}>
+        <React.Fragment>
+          <h2>Button Component Module</h2>
+          <div style={{display:'flex'}}>
+            <div>
+              <label htmlFor='loading'>Loading...</label>
+              <input type='checkbox' id='loading' onChange={this.onChangeBasicProps}></input>
+            </div>
+            <div>
+              <label htmlFor='disabled'>Disabled...</label>
+              <input type='checkbox' id='disabled' onChange={this.onChangeBasicProps}></input>
+            </div>
+            <div>
+              <label htmlFor='light'>Theme Light</label>
+              <input type='radio' id='light' name='theme' onChange={this.onChangeTheme}></input>
+            </div>
+            <div>
+              <label htmlFor='dark'>Theme Dark</label>
+              <input type='radio' id='dark' name='theme' onChange={this.onChangeTheme}></input>
+            </div>
+          </div>
+          <div>
+            {
+              buttonsArray.map((button,index) => {
+                return (
+                  <Button key={index} loading={this.state.loading} disabled={this.state.disabled} {...button.props}>
+                    {button.text}
+                  </Button>
+                )
+              })
+            }
+          </div>
+        </React.Fragment>
+      </ThemeProvider>
+    )
+  }
+}
 
 render(<App />, document.getElementById("root"));
